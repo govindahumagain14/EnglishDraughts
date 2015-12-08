f3c49c64709ec8737ff63514aa0a36a3d84f9d9c
govindahumagain14 <govinda.humagain14@gmail.com>
2015-12-08 18:36:31 +0530

    Highlight of Cell is done.

--- app/src/main/java/draughts/com/englishdraughts/CustomDraughtView.java
+++ app/src/main/java/draughts/com/englishdraughts/CustomDraughtView.java
@@ -6,20 +6,25 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
-import java.util.ArrayList;
-
-public class CustomDraughtView extends View {
+public class CustomDraughtView extends View implements View.OnTouchListener {
     private int heightOfView;
     private int cellHeight;
     private Paint whiteLine = new Paint();
     private Paint redCoin = new Paint();
+    private Paint whiteCoin = new Paint();
     private Rect rect;
     private Paint greenCell = new Paint();
-    private ArrayList<Coin><Coin> game;
+    private Paint black = new Paint();
+    private int[][] game;
+    private int noOfCells = 8;
+    private int mPreviousXPosition;
+    private int mPreviousYPosition;
 
     public CustomDraughtView(Context context) {
         super(context);
@@ -41,13 +46,26 @@ public class CustomDraughtView extends View {
                 .getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
         heightOfView = display.getWidth();
-        cellHeight = heightOfView / 10;
+        this.setOnTouchListener(this);
+        cellHeight = heightOfView / noOfCells;
 
         whiteLine.setStrokeWidth(3);
         whiteLine.setColor(Color.WHITE);
 
         redCoin.setStrokeWidth(3);
         redCoin.setColor(Color.RED);
+        redCoin.setStrokeCap(Paint.Cap.ROUND);
+
+        whiteCoin.setStrokeWidth(3);
+        whiteCoin.setColor(Color.WHITE);
+        whiteCoin.setStrokeCap(Paint.Cap.ROUND);
+
+        black.setColor(Color.BLACK);
+
+       /* redCoin.setShader(new LinearGradient(0.40f, 0.0f, 100.60f, 100.0f,
+                Color.BLACK,
+                Color.BLACK,
+                Shader.TileMode.CLAMP));*/
 
         greenCell.setStrokeWidth(3);
         greenCell.setColor(Color.GREEN);
@@ -55,10 +73,20 @@ public class CustomDraughtView extends View {
     }
 
     private void initiateGame() {
-        game = new ArrayList<>();
-        for(int i=0;i<10;i++) {
-            for (int j = 0; j < 10; j++) {
-game.get[j][i]
+        game = new int[noOfCells][noOfCells];
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game.length; j++) {
+                if (i < 3) {
+                    if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                        game[i][j] = 1;
+                    }
+                } else if (i > game.length - 4) {
+                    if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                        game[i][j] = 2;
+                    }
+                } else {
+                    game[i][j] = 0;
+                }
             }
         }
     }
@@ -75,30 +103,45 @@ game.get[j][i]
         for (int i = 1; i <= 10; i++) {
             canvas.drawLine(cellHeight * i, 0, cellHeight * i, heightOfView, whiteLine);
         }*/
-
-        for(int i=0;i<10;i++,i++){
-            for(int j=0;j<10;j++,j++){
-//Left = where, top=where, right=width of rect, bottom=height of rect
-                rect = new Rect(cellHeight * i, cellHeight * j , cellHeight * i+cellHeight , cellHeight * j + cellHeight);
-
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game.length; j++) {
+                    if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                        rect = new Rect(cellHeight * i, cellHeight * j, cellHeight * i + cellHeight, cellHeight * j + cellHeight);
+                        canvas.drawRect(rect, greenCell);
+                    }
+            }
+        }
+        /*for (int i = 0; i < noOfCells; i++, i++) {
+            for (int j = 0; j < noOfCells; j++, j++) {
+            //Left = where, top=where, right=width of rect, bottom=height of rect
+                rect = new Rect(cellHeight * i, cellHeight * j, cellHeight * i + cellHeight, cellHeight * j + cellHeight);
                 canvas.drawRect(rect, greenCell);
             }
         }
 
-        for(int i=1;i<10;i++,i++){
-            for(int j=1;j<10;j++,j++){
-//Left = where, top=where, right=width of rect, bottom=height of rect
-                rect = new Rect(cellHeight * i, cellHeight * j , cellHeight * i+cellHeight , cellHeight * j + cellHeight);
+        for (int i = 1; i < noOfCells; i++, i++) {
+            for (int j = 1; j < noOfCells; j++, j++) {
+                //Left = where, top=where, right=width of rect, bottom=height of rect
+                rect = new Rect(cellHeight * i, cellHeight * j, cellHeight * i + cellHeight, cellHeight * j + cellHeight);
 
                 canvas.drawRect(rect, greenCell);
             }
-        }
-        for(int i=0;i<10;i++) {
-            for (int j = 0; j < 10; j++) {
+        }*/
+        for (int i = 0; i < noOfCells; i++) {
+            for (int j = 0; j < noOfCells; j++) {
+                // Selected cell
+                if (game[i][j] == 3) {
+                    rect = new Rect(cellHeight * j, cellHeight * i, cellHeight * j + cellHeight, cellHeight * i + cellHeight);
+                    canvas.drawRect(rect, black);
+                }
+                if (game[i][j] == 1) {
+                    canvas.drawCircle(cellHeight / 2 * (j * 2) + cellHeight / 2, cellHeight / 2 * (i * 2) + cellHeight / 2, cellHeight / 4, whiteCoin);
+                } else if (game[i][j] == 2) {
+                    canvas.drawCircle(cellHeight / 2 * (j * 2) + cellHeight / 2, cellHeight / 2 * (i * 2) + cellHeight / 2, cellHeight / 4, redCoin);
+                }
 
             }
         }
-
     }
 
     @Override
@@ -106,20 +149,53 @@ game.get[j][i]
         super.onDraw(canvas);
         // startX, float startY, float stopX, float stopY, Paint paint
         drawTable(canvas);
-       // rect = new Rect(0, 0, cellHeight, cellHeight);
+        // rect = new Rect(0, 0, cellHeight, cellHeight);
         // canvas.drawRect(rect, redCoin);
-        // draw circle
+        // draw circle CX, XY, radius, paint
         // canvas.drawCircle(cellHeight / 2, cellHeight / 2, cellHeight / 4, whiteLine);
         //rect = new Rect(cellHeight, cellHeight, cellHeight * 2, cellHeight * 2);
         //canvas.drawRect(rect, redCoin);
         //left <= right and top <= bottom).
         //Left = where, top=where, right=width of rect, bottom=height of rect
-      //  rect = new Rect(cellHeight * 2, cellHeight * 2+cellHeight, cellHeight * 2 , cellHeight * 2 + cellHeight);
+        //  rect = new Rect(cellHeight * 2, cellHeight * 2+cellHeight, cellHeight * 2 , cellHeight * 2 + cellHeight);
 
-       // canvas.drawRect(rect, redCoin);
+        // canvas.drawRect(rect, redCoin);
         //canvas.drawLine(0, 0, heightOfView, heightOfView, whiteLine);
-       // rect = new Rect(cellHeight, 0, cellHeight*2, cellHeight);
-       // canvas.drawRect(rect, redCoin);
+        // rect = new Rect(cellHeight, 0, cellHeight*2, cellHeight);
+        // canvas.drawRect(rect, redCoin);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int touchXmax = cellHeight * 2 * noOfCells;
+        int touchYmax = cellHeight * 2 * noOfCells;
+        int touchXmin = 4;
+        int touchYmin = 150;
+        float touchX = event.getRawX();
+        float touchY = event.getRawY() - 150;
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            Log.i("srikanth", "Cell ::x:: " + (int) (touchX / (cellHeight))
+                    + " Y:: " + (int) (touchY / (cellHeight)));
+            Log.i("srikanth", "touch up" + event.getRawX() + "touch Y :: "
+                    + event.getRawY());
+
+            if (touchX > touchXmin && touchX < touchXmax && touchY > touchYmin
+                    && touchY < touchYmax) {
+                Log.i("srikanth", "Rect size:: " + cellHeight);
+            }
+            int yPosition = (int) (touchX / (cellHeight));
+            int xPosition = (int) (touchY / (cellHeight));
+            if (yPosition >= 0 && xPosition <= 7) {
+
+                Log.i("srikanth", "Rect x:: " + yPosition +" y:: "+xPosition);
+                mPreviousXPosition = xPosition;
+                mPreviousYPosition = yPosition;
+                game[xPosition][yPosition] = 3;
+
+               invalidate();
+            }
+        }
+        return true;
     }
 
 
