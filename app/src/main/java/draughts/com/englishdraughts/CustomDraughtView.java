18e10566e02fb7ffdbd9527e670ac2293f899e58
govindahumagain14 <govinda.humagain14@gmail.com>
2015-12-10 19:30:14 +0530

    Direct move for red coin is done.

--- app/src/main/java/draughts/com/englishdraughts/CustomDraughtView.java
+++ app/src/main/java/draughts/com/englishdraughts/CustomDraughtView.java
@@ -30,10 +30,13 @@ public class CustomDraughtView extends View implements View.OnTouchListener {
     private boolean isWhiteTurn = false;
     private String TAG = "View";
     private float minYPixels;
-    int whiteCoinValue = 2;
-    int redCoinValue = 1;
+    private int intialValue = 0;
+    private int redCoinValue = 1;
+    private int whiteCoinValue = 2;
     private int selectedPositionValue = 3;
+    private int possibleMoveValue = 4;
     private int selectedCoinValue;
+    private boolean isValidTouch;
 
 
     public CustomDraughtView(Context context) {
@@ -71,6 +74,7 @@ public class CustomDraughtView extends View implements View.OnTouchListener {
         whiteCoin.setStrokeCap(Paint.Cap.ROUND);
 
         black.setColor(Color.BLACK);
+        black.setStrokeWidth(20);
 
         greenCell.setStrokeWidth(3);
         greenCell.setColor(Color.GREEN);
@@ -92,7 +96,7 @@ public class CustomDraughtView extends View implements View.OnTouchListener {
                         game[i][j] = redCoinValue;
                     }
                 } else {
-                    game[i][j] = 0;
+                    game[i][j] = intialValue;
                 }
             }
         }
@@ -102,10 +106,10 @@ public class CustomDraughtView extends View implements View.OnTouchListener {
     private void drawTable(Canvas canvas) {
         for (int i = 0; i < game.length; i++) {
             for (int j = 0; j < game.length; j++) {
-                    if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
-                        rect = new Rect(cellHeight * i, cellHeight * j, cellHeight * i + cellHeight, cellHeight * j + cellHeight);
-                        canvas.drawRect(rect, greenCell);
-                    }
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                    rect = new Rect(cellHeight * i, cellHeight * j, cellHeight * i + cellHeight, cellHeight * j + cellHeight);
+                    canvas.drawRect(rect, greenCell);
+                }
             }
         }
         for (int i = 0; i < noOfCells; i++) {
@@ -114,9 +118,9 @@ public class CustomDraughtView extends View implements View.OnTouchListener {
                 if (game[i][j] == selectedPositionValue) {
                     rect = new Rect(cellHeight * j, cellHeight * i, cellHeight * j + cellHeight, cellHeight * i + cellHeight);
                     canvas.drawRect(rect, black);
-                    if(selectedCoinValue == whiteCoinValue){
+                    if (selectedCoinValue == whiteCoinValue) {
                         canvas.drawCircle(cellHeight / 2 * (j * 2) + cellHeight / 2, cellHeight / 2 * (i * 2) + cellHeight / 2, cellHeight / 4, whiteCoin);
-                    } else if (selectedCoinValue == redCoinValue){
+                    } else if (selectedCoinValue == redCoinValue) {
                         canvas.drawCircle(cellHeight / 2 * (j * 2) + cellHeight / 2, cellHeight / 2 * (i * 2) + cellHeight / 2, cellHeight / 4, redCoin);
                     }
                 }
@@ -126,6 +130,9 @@ public class CustomDraughtView extends View implements View.OnTouchListener {
                     canvas.drawCircle(cellHeight / 2 * (j * 2) + cellHeight / 2, cellHeight / 2 * (i * 2) + cellHeight / 2, cellHeight / 4, redCoin);
                 }
 
+                if (game[i][j] == possibleMoveValue) {
+                    canvas.drawCircle(cellHeight / 2 * (j * 2) + cellHeight / 2, cellHeight / 2 * (i * 2) + cellHeight / 2, cellHeight / 6, black);
+                }
             }
         }
     }
@@ -151,88 +158,159 @@ public class CustomDraughtView extends View implements View.OnTouchListener {
             }
             int yPosition = (int) (touchX / (cellHeight));
             int xPosition = (int) (touchY / (cellHeight));
-            Log.i(TAG, "X :: "+xPosition+" Y:: "+yPosition);
+            Log.i(TAG, "X :: " + xPosition + " Y:: " + yPosition);
+            Log.i(TAG, "Value:: "+game[xPosition][yPosition]);
             if (yPosition >= 0 && xPosition < noOfCells) {
                 // Coin is not selected
-                if(mPreviousYPosition == -1) {
-                    if(game[xPosition][yPosition] != whiteCoinValue && game[xPosition][yPosition] != redCoinValue){
+                if (mPreviousYPosition == -1) {
+                    if (game[xPosition][yPosition] != whiteCoinValue && game[xPosition][yPosition] != redCoinValue) {
                         return true;
                     }
                     // Is white turn
-                   // if (isWhiteTurn && game[xPosition][yPosition] == whiteCoinValue){
-                        setSelectedPosition(xPosition, yPosition);
+                    if (isWhiteTurn && game[xPosition][yPosition] == whiteCoinValue) {
+                        setWhiteSelectedPosition(xPosition, yPosition);
+                        isValidTouch = true;
+                    } else if (!isWhiteTurn && game[xPosition][yPosition] == redCoinValue) {
+                        setRedSelectedPosition(xPosition, yPosition);
+                        getPossibleMovementsForRed(xPosition, yPosition);
                         invalidate();
-                   /* } else if(!isWhiteTurn && game[xPosition][yPosition] == redCoinValue){
-                        resetCurrentPosition();
-                        invalidate();
-                    }*/
+                    }
                 }
                 // Coin is selected
-                else {
-                    if (mPreviousXPosition == xPosition && mPreviousYPosition == yPosition){
-                        resetCurrentPosition();
-                        invalidate();
-                    }
+                else if (game[xPosition][yPosition] == selectedPositionValue) {
+                    resetCurrentPosition(xPosition, yPosition);
+                    isValidTouch = true;
+                    invalidate();
+                    return true;
                 }
-                /*if( ){
+                // Check for coin movement and move to that position
+                if (game[xPosition][yPosition] == possibleMoveValue) {
+                    // TODO: If valid movement, turn the player from white to red and vice versa
+                    if (!isWhiteTurn) {
+                        game[xPosition][yPosition] = redCoinValue;
 
-                } game[xPosition][yPosition] == whiteCoinValue) {
-                    // If coin is not selected
-                    if(mPreviousXPosition == -1) {
-
-                    }
-                    // If previous selected position is same selection, deselect the coin
-                    else if (mPreviousXPosition == xPosition && mPreviousYPosition == yPosition){
-                        resetCurrentPosition();
-                        invalidate();
-                    }
-                    // Check is valid move and move the coin
-                    else {
+                    } else {
 
                     }
+                    isValidTouch = true;
+                    game[mPreviousXPosition][mPreviousYPosition] = intialValue;
+                    isWhiteTurn = !isWhiteTurn;
+                    mPreviousYPosition = -1;
                 }
-                // Is Red turn
-                else if (!isWhiteTurn && game[xPosition][yPosition] == redCoinValue) {
-                    // If coin is not selected
-                    if(mPreviousXPosition == -1) {
-                        setSelectedPosition(xPosition, yPosition);
-                        invalidate();
-                    }
-                    // If previous selected position is same selection, deselect the coin
-                    else if (mPreviousXPosition == xPosition && mPreviousYPosition == yPosition){
-                        resetCurrentPosition();
-                        invalidate();
-                    }
-                    // Check is valid move and move the coin
-                    else {
-
-                    }
-                }*/
+                if (isValidTouch) {
+                    invalidate();
+                }
             }
         }
         return true;
     }
+    private void getPossibleMovementsForWhite(int xPosition, int yPosition) {
+        // Is King
+        if (false) {
 
-    private void resetCurrentPosition() {
-        if(isWhiteTurn){
-            game[mPreviousXPosition][mPreviousYPosition] = whiteCoinValue;
         } else {
-            game[mPreviousXPosition][mPreviousYPosition] = redCoinValue;
+            // Check for right side moves
+            checkRightRedPossibleMoves(xPosition, yPosition);
+            // Check for left side moves
+            checkLeftRedPossibleMoves(xPosition, yPosition);
+        }
+    }
+
+    private void getPossibleMovementsForRed(int xPosition, int yPosition) {
+        // Is King
+        if (false) {
+
+        } else {
+            // Check for right side moves
+            checkRightRedPossibleMoves(xPosition, yPosition);
+            // Check for left side moves
+            checkLeftRedPossibleMoves(xPosition, yPosition);
+        }
+    }
+
+    private void checkRightRedPossibleMoves(int xPosition, int yPosition) {
+        for (int i = xPosition - 1; i >= 0; i--) {
+            for (int j = yPosition + 1; j < noOfCells; j++) {
+
+                Log.i(TAG, " X right:: " + i);
+                // If white coin exists
+                if (game[i][j] == whiteCoinValue) {
+                    // Check for empty space cross to this
+                }
+                // else, red coin exists
+                else if (game[i][j] == redCoinValue) {
+                    return;
+                }
+                // Empty space
+                else if (game[i][j] != possibleMoveValue) {
+                    Log.i(TAG, "pOSS right: X:: " + (i) + " Y:: " + (j));
+                    game[i][j] = possibleMoveValue;
+                    return;
+                }
+                // Possible value
+                else if (game[i][j] == possibleMoveValue) {
+                    return;
+                }
+            }
+        }
+    }
+
+    private void checkLeftRedPossibleMoves(int xPosition, int yPosition) {
+        for (int i = xPosition - 1; i >= 0; i--) {
+            for (int j = yPosition - 1; j >= 0; j--) {
+                Log.i(TAG, " X left:: " + i);
+                // If white coin exists
+                if (game[i][j] == whiteCoinValue) {
+                    // Check for empty space cross to this
+                }
+                // else, red coin exists
+                else if (game[i][j] == redCoinValue) {
+                    return;
+                }
+                // Empty space;
+                else if (game[i][j] != possibleMoveValue) {
+                    Log.i(TAG, "pOSS left: X:: " + (i) + " Y:: " + (j));
+                    game[i][j] = possibleMoveValue;
+                    return;
+                }
+                // Possible value
+                else if (game[i][j] == possibleMoveValue) {
+                    return;
+                }
+            }
+        }
+    }
+
+    private void resetCurrentPosition(int xPosition, int yPosition) {
+        if (isWhiteTurn) {
+            game[xPosition][yPosition] = whiteCoinValue;
+        } else {
+            game[xPosition][yPosition] = redCoinValue;
+        }
+        for (int i = 0; i < noOfCells; i++) {
+            for (int j = 0; j < noOfCells; j++) {
+                if (game[i][j] == possibleMoveValue) {
+                    game[i][j] = intialValue;
+                }
+            }
         }
         mPreviousXPosition = -1;
         mPreviousYPosition = -1;
         selectedCoinValue = -1;
     }
 
-    private void setSelectedPosition(int xPosition, int yPosition) {
+    private void setWhiteSelectedPosition(int xPosition, int yPosition) {
         mPreviousXPosition = xPosition;
         mPreviousYPosition = yPosition;
         game[xPosition][yPosition] = 3;
-        if(isWhiteTurn){
-            selectedCoinValue = whiteCoinValue;
-        } else {
-            selectedCoinValue = redCoinValue;
-        }
-
+        selectedCoinValue = whiteCoinValue;
     }
+
+    private void setRedSelectedPosition(int xPosition, int yPosition) {
+        mPreviousXPosition = xPosition;
+        mPreviousYPosition = yPosition;
+        game[xPosition][yPosition] = 3;
+        selectedCoinValue = redCoinValue;
+    }
+
 }
